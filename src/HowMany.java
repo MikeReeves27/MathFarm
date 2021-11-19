dc9909b1a72e0f1273541699922e49318b84f145
Michael Reeves <62658283+MikeReeves27@users.noreply.github.com>
2021-11-19 01:16:37 -0500

    Update HowMany.java

--- src/HowMany.java
+++ src/HowMany.java
@@ -6,24 +6,33 @@ public class HowMany {
 	private String questionText;
 	private String element;
 	private String image;
+	private double imageRatio;
 	private int correctAnswer;
+	private int correctAnswerIndex;
 	private int range;
 	private int wrongAnswer1;
 	private int wrongAnswer2;
-	private int[] answers;
+	private String[] answers;
 
 	Random rand = new Random();
 
 	public HowMany(int grade) {
 		this.grade = grade;
+
+		// Declare element names and their image ratios
 		String[] elements = { "lambs", "roosters", "piggies" };
-		element = elements[rand.nextInt(elements.length)];
+		double[] imageRatios = { 0.93, 0.83, 1.20 };
+
+		// Choose an element from the array at random
+		int index = rand.nextInt(elements.length);
+		element = elements[index];
+		imageRatio = imageRatios[index];
 		image = "/" + element + ".png";
 		questionText = "How many " + element + " are there?";
 
-		// If K grade is chosen, set range to 5
+		// If K grade is chosen, set range to 10
 		if (grade == 0) {
-			range = 5;
+			range = 10;
 		}
 
 		// Randomly assign 1 correct answer and 2 wrong answers
@@ -38,25 +47,26 @@ public class HowMany {
 		}
 		while (true) {
 			int answer = rand.nextInt(range) + 1;
-			if (answer != correctAnswer && answer != wrongAnswer2) {
-				wrongAnswer1 = answer;
+			if (answer != correctAnswer && answer != wrongAnswer1) {
+				wrongAnswer2 = answer;
 				break;
 			}
 		}
 
-		// Randomly assign all 3 answers to different indicies in array
-		// Run loops to ensure all 3 are in unique places
-		answers = new int[3];
-		answers[rand.nextInt(answers.length)] = correctAnswer;
+		// Randomly assign all 3 answers to different indicies in array. Run loops to
+		// ensure all 3 are in unique places
+		answers = new String[3];
+		correctAnswerIndex = rand.nextInt(answers.length);
+		answers[correctAnswerIndex] = String.valueOf(correctAnswer);
 		for (int i = 0; i < answers.length; i++) {
-			if (answers[i] == 0) {
-				answers[i] = wrongAnswer1;
+			if (answers[i] == null) {
+				answers[i] = String.valueOf(wrongAnswer1);
 				break;
 			}
 		}
 		for (int i = 0; i < answers.length; i++) {
-			if (answers[i] == 0) {
-				answers[i] = wrongAnswer2;
+			if (answers[i] == null) {
+				answers[i] = String.valueOf(wrongAnswer2);
 				break;
 			}
 		}
@@ -71,11 +81,19 @@ public class HowMany {
 		return image;
 	}
 
+	public double getImageRatio() {
+		return imageRatio;
+	}
+
 	public int getCorrectAnswer() {
 		return correctAnswer;
 	}
 
-	public int[] getAnswers() {
+	public int getCorrectAnswerIndex() {
+		return correctAnswerIndex;
+	}
+
+	public String[] getAnswers() {
 		return answers;
 	}
 
